ceeeb9a1104d78c474f4ed3188c1d6288747eb69
Colleen O'Briant <59625715+cobriant@users.noreply.github.com>
2021-11-15 07:08:45 -0800

    Add files via upload
    K19 and 20

---
+++ R/K19_reduce.R
@@ -0,0 +1,174 @@
+#-------------------------------------------------------------------------
+#            Intro to the Tidyverse by Colleen O'Briant
+#                       Koan #19: reduce()
+#-------------------------------------------------------------------------
+
+# In order to progress:
+# 1. Read all instructions carefully.
+# 2. When you come to an exercise, fill in the blank, un-comment the line,
+#    and execute the code in the console (Ctrl/Cmd Return). If the piece
+#    of code spans multiple lines, highlight the whole chunk or simply put
+#    your cursor at the end of the last line.
+# 3. Test that your answers are correct (Ctrl/Cmd Shift T)
+# 4. Save (Ctrl/Cmd S).
+
+#-------------------------------------------------------------------------
+
+# We've already learned the first function from the purrr family: map().
+# Now we'll learn the next two: in this koan, we'll focus on reduce(),
+# and in koan 20, we'll learn accumulate().
+
+# map(.x, .f) applies a function `.f` to every element of `.x` and creates a
+# list of outputs the same length as `.x`.
+
+# Similarly, reduce(.x, .f) applies a 2-argument function `.f` to every
+# element of `.x` *sequentially* to get you *one* final output. That is, it uses
+# `.f` to reduce `.x`.
+
+# In this koan, we'll see how max() reduces a vector down to a single output,
+# and also that min() and sum() do the same thing.
+
+library(tidyverse)
+
+# The first example of how `reduce()` can be used is the function `max()`.
+# `max()` takes a vector of numbers and returns the number which is the largest.
+
+# 1. Fill in the blank below so that the statement returns TRUE:
+
+#1@
+
+max(c(5, 3, __, 2)) == 9
+
+#@1
+
+# What if you had to write the function `max` from scratch? You would probably
+# need to use `>`, keeping track of the maximum you've seen so far. So an
+# algorithm to sequentially apply `>` to find the maximum of c(4, 2, 6, 8) would
+# be this:
+
+# Compare the first two numbers in `.x`:
+4 > 2
+# Since this returns TRUE, save 4 as the largest number seen thus far.
+
+# Compare 4 with the next number in `.x`:
+4 > 6
+# Since this returns FALSE, save 6 as the largest number seen thus far.
+
+# Compare 6 with the next number in `.x`:
+6 > 8
+# Since this returns FALSE, save 8 as the largest number seen thus far.
+# Return 8 since we've made it through `.x`.
+
+# I'll write a 2-argument function that takes two numbers, compares them,
+# and returns the larger one. I'll call this function "larger", and I'll
+# use if_else() to do it:
+
+?qelp::if_else
+
+larger <- function(x, y) {
+  if_else(x > y, x, y)
+}
+
+# 2. Test that the function works by filling in the blank below
+# with any number to make the statement return TRUE:
+
+#2@
+
+larger(5, __) == 5
+
+#@2
+
+# `larger` is a good start, but I want `max` to work on a vector of any size,
+# not just 2. Something I could do is to apply `larger` until `.x` is through,
+# sequentially:
+
+larger(4, 2) %>%
+  larger(6) %>%
+  larger(8)
+
+# Using `reduce()` does the same thing as what I've written above to sequentially
+# apply `.f` until `.x` is through:
+
+reduce(c(4, 2, 6, 8), larger)
+
+# So I can write a function my_max() that takes any vector of any length and finds
+# what the maximum is by sequentially applying `larger`:
+
+my_max <- function(x) {
+  reduce(x, larger)
+}
+
+# Check that the function works:
+
+my_max(c(4, 2, 6, 8))
+
+# Instead of creating the functions `larger` and `my_max`, if you
+# just wanted to find the maximum of a vector using reduce(), you
+# can use the formula syntax just like with map(). Since .f is a
+# 2-argument function for reduce(), you'll refer to `.x` AND `.y`.
+# .x is the value passed from previous evaluations of .f, and .y
+# refers to the new element of the vector.
+
+reduce(c(4, 2, 6, 8), ~ if_else(.x > .y, .x, .y))
+
+# This is a great visualization of what `reduce()` does from Hadley Wickham's
+# book *Advanced R*:
+#' https://d33wubrfki0l68.cloudfront.net/9c239e1227c69b7a2c9c2df234c21f3e1c74dd57/eec0e/diagrams/functionals/reduce.png
+
+# Also read the qelp docs for reduce:
+?qelp::reduce
+
+#-------------------------------------------------------------------------
+
+# 3. Your turn: create a function `my_min` that finds the minimum
+# of a vector of numbers, using `<`, `if_else`, and `reduce`.
+
+#3@
+
+my_min <- function(x) {
+  __
+}
+
+#@3
+
+# Check that my_min works:
+
+my_min(c(4, 2, 6, 8))
+
+#-------------------------------------------------------------------------
+
+# So we've learned that you can think of max() as a reduced `>` and
+# min() as a reduced `<`. You can also think of sum() as a reduced `+`.
+
+# sum() takes a vector and sequentially applies `+` until .x is through:
+sum(1:10)
+
+# a + b is actually just syntactic sugar for this: `+`(a, b): you're applying
+# the 2-argument function `+` to a and b. So sum(1:10) does this:
+
+`+`(1, 2) %>%
+  `+`(3) %>%
+  `+`(4) %>%
+  `+`(5) %>%
+  `+`(6) %>%
+  `+`(7) %>%
+  `+`(8) %>%
+  `+`(9) %>%
+  `+`(10)
+
+# 4. Use `+` and `reduce` to write a function that finds the sum of any vector.
+
+#4@
+
+my_sum <- function(x) {
+  __
+}
+
+#@4
+
+my_sum(1:1000) == sum(1:1000)
+
+#-------------------------------------------------------------------------
+
+# Great work! You're one step closer to tidyverse enlightenment.
+# Make sure to return to this topic to meditate on it later.
---
+++ R/K20_accumulate.R
@@ -0,0 +1,156 @@
+#-------------------------------------------------------------------------
+#            Intro to the Tidyverse by Colleen O'Briant
+#                       Koan #20: accumulate()
+#-------------------------------------------------------------------------
+
+# In order to progress:
+# 1. Read all instructions carefully.
+# 2. When you come to an exercise, fill in the blank, un-comment the line,
+#    and execute the code in the console (Ctrl/Cmd Return). If the piece
+#    of code spans multiple lines, highlight the whole chunk or simply put
+#    your cursor at the end of the last line.
+# 3. Test that your answers are correct (Ctrl/Cmd Shift T)
+# 4. Save (Ctrl/Cmd S).
+
+#-------------------------------------------------------------------------
+
+# The last function we'll learn from the purrr family is accumulate().
+# accumulate() works just like `reduce` to sequentially apply a 2-argument
+# function to `.x`, but the difference is that accumulate() outputs the
+# intermediate results so that the output is the same length as `.x`, just
+# like map(). The last element of the accumulate() output will be the same
+# as the reduce() output.
+
+# This function shows you the computations from the max() algorithm
+# we discussed in the previous koan. Run this code to see the result:
+
+accumulate(c(1, 3, 9, 2), ~ if_else(.x > .y, .x, .y))
+
+# The first element of the accumulate() output from above is a 1.
+# `.f` hasn't been applied yet. In order to make the output the same length
+# as the input, accumulate() just outputs the first element of `.x` as-is.
+
+# The second element of the accumulate output is 3. That's because
+# the maximum of 1 and 3 is 3.
+
+# The third element of the accumulate output is 9. That's because
+# the maximum of 3 and 9 is 9.
+
+# The fourth element of the accumulate output is 9. That's because
+# the maximum of 9 and 2 is 9.
+
+#-------------------------------------------------------------------------
+
+# Now consider the minimum function:
+
+accumulate(1:10, ~ if_else(.x < .y, .x, .y))
+
+# 1. Fill in the blank so that this statement returns TRUEs:
+
+#1@
+
+accumulate(c(__, 11, __, __), ~ if_else(.x < .y, .x, .y)) == c(7, 7, __, 2)
+
+#@1
+
+#-------------------------------------------------------------------------
+
+# Consider the sum() function:
+
+accumulate(1:10, `+`)
+
+# 2. Fill in the blank so that this statement returns TRUEs:
+
+#2@
+
+accumulate(c(__, __, __, __), ~ .x + .y) == c(3, 10, 0, 2)
+
+#@2
+
+#-------------------------------------------------------------------------
+
+#' In order to generate data that follows a random walk:
+#' $y_t = y_{t-1} + u_t$,
+#' you can use an accumulated sum of $u_t$:
+
+accumulate(c(0, -1, 1, 2, -1), ~ .x + .y)
+
+# This is what's happening to get the output from the above code:
+c(
+  0,
+  0 + -1,
+  0 + -1 + 1,
+  0 + -1 + 1 + 2,
+  0 + -1 + 1 + 2 + -1
+)
+
+# You can also generate u_t from the normal distribution:
+
+accumulate(rnorm(n = 10), ~ .x + .y)
+
+# Let's visualize the random walk! Run the code below a couple of times
+# to see that random walks aren't more likely to trend downward or upward.
+# Time series like stocks or housing prices (whenever the data generating
+# process includes a lot of speculation) are modeled well with random walks.
+# That's because the best guess anyone can make about a stock's value next period
+# is the stock's value this period. If we could do better with our prediction,
+# we could make a lot of money. But the point is, nobody can!
+
+tibble(
+  t = 1:100,
+  y = accumulate(rnorm(n = 100), ~ .x + .y)
+) %>%
+  ggplot(aes(x = t, y = y)) +
+  geom_line()
+
+# 3. Take the first difference of this random walk:
+
+#3@
+
+tibble(
+  u = c(1, 0, 0, 1, -1, -2, 1, 0),
+  y = accumulate(u, ~ .x + .y)
+) %>%
+  mutate(y_diff = __)
+
+#@3
+
+# Notice that the first difference will recover the vector u (without
+# the first term). This happens because:
+
+#' $y_t = y_{t+1} + u_t$
+#' $y_t - y_{t+1} = u_t$
+
+#-------------------------------------------------------------------------
+
+#' You can also simulate other autocorrelated time series using accumulate():
+#' Instead of a random walk: $y_t = y_{t-1} + u_t$,
+#' Suppose we want to simulate: $y_t = \beta_0 + \beta_1 y_{t-1} + u_t$.
+
+#' 4. Generate an autocorrelated time series:
+#' $y = .5 + .5*y_{t-1} + u_t$, u_t ~ iid N(0, $\sigma^2$)
+
+#4@
+
+accumulate(c(1, 0, __, -1), ~ .5 + .5*.x + .y) == c(1, 1, 3, __)
+
+#@4
+
+# Visualize the time series:
+
+tibble(
+  t = 1:100,
+  y = accumulate(rnorm(n = 100), ~ .5 + .5*.x + .y)
+) %>%
+  ggplot(aes(x = t, y = y)) +
+  geom_line()
+
+#-------------------------------------------------------------------------
+
+# Wow! You've finished all 20 tidyverse koans, what a wonderful
+# accomplishment. You've worked hard and learned so much.
+
+# You may not feel like you've achieved tidyverse enlightenment yet, but
+# as you keep practicing, you may find yourself surprised to learn just
+# how powerful these concepts truly are. And you may discover you are a
+# much more powerful programmer than you ever thought you could be!
