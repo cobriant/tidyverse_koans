a64cf7431d7aa9f2cef174068b5e262aa8b6c1c4
Colleen O'Briant <59625715+cobriant@users.noreply.github.com>
2026-01-11 11:25:14 -0800

    Update test-K09_ggplot_aes.R

--- tests/testthat/test-K09_ggplot_aes.R
+++ tests/testthat/test-K09_ggplot_aes.R
@@ -9,25 +9,29 @@ test_that("K09: Question 1", {
 test_that("K09: Question 2", {
   skip_incomplete(K09, 2)
   p <- parse_eval(str_match_q(K09, 2))
-  expect_equal(p$labels$colour, "continent")
+  expect_equal(rlang::as_label(p$mapping$colour), "continent")
 })
 
 test_that("K09: Question 3", {
   skip_incomplete(K09, 3)
   p <- parse_eval(str_match_q(K09, 3))
-  expect_equal(p$labels$colour, "continent")
-  expect_equal(p$labels$fill, "year")
+
+  expect_equal(rlang::as_label(p$mapping$colour), "continent")
+  expect_equal(rlang::as_label(p$mapping$fill),   "year")
 })
 
 test_that("K09: Question 4", {
   skip_incomplete(K09, 4)
   p <- parse_eval(str_match_q(K09, 4))
-  expect_equal(p$labels$shape, "continent")
+
+  expect_equal(rlang::as_label(p$mapping$shape), "continent")
 })
 
 test_that("K09: Question 5", {
   skip_incomplete(K09, 5)
   p <- parse_eval(str_match_q(K09, 5))
-  expect_equal(p$labels$colour, "continent")
-  expect_equal(p$labels$size, "pop")
+
+  expect_equal(rlang::as_label(p$mapping$colour), "continent")
+  expect_equal(rlang::as_label(p$mapping$size),   "pop")
 })
+
